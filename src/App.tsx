c10af1338aa90a7b0599685aac1f33e268bbf252
wildbook7 <wildbook7@gmail.com>
2022-05-08 21:21:13 +0900

    muiの準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <Button variant="contained">Hello mui</Button>
+          <br />
           Edit <code>src/App.tsx</code>(v2) and save to reload.
         </p>
         <a
