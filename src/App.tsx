d0369dca1ffe3797fdfaa815265c67c1967d6647
wildbook7 <wildbook7@gmail.com>
2022-05-15 18:55:04 +0900

    イチゴの表示切替

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import strawberry from './strawberry.png';
 import './App.css';
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import { styled } from '@mui/material/styles'
+import { Grid } from '@mui/material';
 
 const CustomPaper = styled(Paper)(() => ({
   backgroundColor: 'khaki',
@@ -13,23 +14,50 @@ const CustomPaper = styled(Paper)(() => ({
 }))
 
 function App() {
+  const [strawberryFlag, setStrawberryFlag] = useState(false)
+
+  const change = () => setStrawberryFlag(!strawberryFlag)
+
   return (
     <div className="App">
       <header className="App-header">
         パンケーキを右の皿へ移そう！
-        <img src={strawberry} alt="strawberry" style={{
-          width: "60px",
-          position: "relative",
-          top: "12px",
-          left: "3px",
-        }} />
-        <Stack spacing={0.3} alignItems="center">
-          <CustomPaper sx={{ width: '50px' }}>&nbsp;</CustomPaper>
-          <CustomPaper sx={{ width: '75px' }}>&nbsp;</CustomPaper>
-          <CustomPaper sx={{ width: '100px' }}>&nbsp;</CustomPaper>
-        </Stack>
+        <Grid container spacing={2} sx={{ width: '800px' }}>
+          <Grid item xs={2.1}></Grid>
+          <Grid item xs={2.6}>
+          <Stack spacing={0.3} alignItems={'center'} justifyContent={'flex-end'} sx={{ height: '120px' }}>
+              <CustomPaper sx={{ width: '50px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '75px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '100px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '125px' }}>&nbsp;</CustomPaper>
+            </Stack>
+          </Grid>
+          <Grid item xs={2.6}>
+          <Stack spacing={0.3} alignItems={'center'} justifyContent={'flex-end'} sx={{ height: '120px' }}>
+              <CustomPaper sx={{ width: '50px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '75px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '100px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '125px' }}>&nbsp;</CustomPaper>
+            </Stack>
+          </Grid>
+          <Grid item xs={2.6}>
+            <Stack spacing={0.3} alignItems={'center'} justifyContent={'flex-end'} sx={{ height: '120px' }}>
+              <img src={strawberry} alt="strawberry" style={{
+                width: "60px",
+                position: "relative",
+                top: "13px",
+                left: "5px",
+                visibility: strawberryFlag? "visible" : "hidden",
+              }} />
+              <CustomPaper sx={{ width: '50px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '75px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '100px' }}>&nbsp;</CustomPaper>
+              <CustomPaper sx={{ width: '125px' }}>&nbsp;</CustomPaper>
+            </Stack>
+          </Grid>
+        </Grid>
         <p>
-          <Button variant="contained">再チャレンジ</Button>
+          <Button variant="contained" onClick={ change }>再チャレンジ</Button>
           <br />
         </p>
         <p>&nbsp;</p>
