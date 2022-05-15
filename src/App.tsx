b1e1533f242f332d6da8205a31f1bbf13cd41f89
wildbook7 <wildbook7@gmail.com>
2022-05-15 13:10:05 +0900

    パンケーキを用意

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,44 @@ import React from 'react';
 import strawberry from './strawberry.png';
 import './App.css';
 import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         パンケーキを右の皿へ移そう！
-        <img src={strawberry} alt="strawberry" style={{ width: "50px"}} />
+        <img src={strawberry} alt="strawberry" style={{
+          width: "60px",
+          position: "relative",
+          top: "12px",
+          left: "3px",
+        }} />
+        <Paper elevation={2} sx={{
+          backgroundColor: 'wheat',
+          border: '1px brown',
+          width: '50px',
+        }}>
+          &nbsp;
+        </Paper>
+        <Paper elevation={2} sx={{
+          backgroundColor: 'wheat',
+          border: '1px brown',
+          width: '75px',
+        }}>
+          &nbsp;
+        </Paper>
+        <Paper elevation={2} sx={{
+          backgroundColor: 'wheat',
+          border: '1px brown',
+          width: '100px',
+        }}>
+          &nbsp;
+        </Paper>
         <p>
           <Button variant="contained">再チャレンジ</Button>
           <br />
         </p>
-        <p>&nbsp;</p>
         <p>&nbsp;</p>
         <p>&nbsp;</p>
         <p>&nbsp;</p>
